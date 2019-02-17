bb5beaf26c3892a1ed5011ad2e1ebd3592ee7700
Paweł Chrzaniuk <pawel.chrzaniuk@pl.ey.com>
2019-02-17 20:48:23 +0100

    solution - section 3, ex. 1

---
+++ Vidly/Migrations/201902171932375_AddRecordsWithMembershipTypes.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRecordsWithMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET [MembershipTypeName] = 'Pay as You Go' WHERE [ID] = 1");
+            Sql("UPDATE MembershipTypes SET [MembershipTypeName] = 'Monthly' WHERE [ID] = 2");
+            Sql("UPDATE MembershipTypes SET [MembershipTypeName] = 'Quarterly' WHERE [ID] = 3");
+            Sql("UPDATE MembershipTypes SET [MembershipTypeName] = 'Annual' WHERE [ID] = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
