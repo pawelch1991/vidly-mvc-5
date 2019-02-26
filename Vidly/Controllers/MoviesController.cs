3488b5840e33344152e7192a625a36ee095308b3
Paweł Chrzaniuk <pawel.chrzaniuk@pl.ey.com>
2019-02-26 19:03:40 +0100

    Updating database

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,14 +35,10 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        //private IEnumerable<Movie> GetMovies()
-        //{
-        //    return new List<Movie>
-        //    {
-        //        new Movie { Id = 1, Name = "Shrek" },
-        //        new Movie { Id = 2, Name = "Wall-e" }
-        //    };
-        //}
+        public ActionResult New()
+        {
+            return View();
+        }
 
         // GET: Movies/Random
         public ActionResult Random()
