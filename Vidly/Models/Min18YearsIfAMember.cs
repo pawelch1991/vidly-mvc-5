664f21fe1002471e7e7fc46fc436ecd71389aaca
Paweł Chrzaniuk <pawel.chrzaniuk@pl.ey.com>
2019-03-03 22:34:45 +0100

    anti-forgery token

--- Vidly/Models/Min18YearsIfAMember.cs
+++ Vidly/Models/Min18YearsIfAMember.cs
@@ -12,7 +12,7 @@ namespace Vidly.Models
         {
             var customer = (Customer) validationContext.ObjectInstance;    
 
-            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1)
+            if (customer.MembershipTypeId == MembershipType.Unknown || customer.MembershipTypeId == MembershipType.PayAsYouGo)
                 return ValidationResult.Success;
 
             var age = DateTime.Today.Year - customer.Birthdate.Value.Year;
