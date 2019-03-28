1c3c9bd0674f8b12ae2150bb639c3bbb74c0f947
Paweł Chrzaniuk <pawel.chrzaniuk@pl.ey.com>
2019-03-28 23:54:21 +0100

    IHttpActionResult implemented in API

--- Vidly/Controllers/Api/CustomersController.cs
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -36,10 +36,10 @@ namespace Vidly.Controllers.Api
 
         // POST / api/customers
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
+                return BadRequest();
 
             var customer = Mapper.Map<CustomerDto,Customer>(customerDto);
             
@@ -48,7 +48,7 @@ namespace Vidly.Controllers.Api
 
             customerDto.Id = customer.Id;
 
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
 
         // PUT /api/customers/1
