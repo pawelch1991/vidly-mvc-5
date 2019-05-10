ba3b12f120501763d60a4a71540dfc54f83ee771
Paweł Chrzaniuk <pawel.chrzaniuk@outlook.com>
2019-05-11 00:01:33 +0200

    phone number added to registration form

--- Vidly/Models/AccountViewModels.cs
+++ Vidly/Models/AccountViewModels.cs
@@ -75,6 +75,11 @@ namespace Vidly.Models
         public string DrivingLicense { get; set; }
 
         [Required]
+        [StringLength(32)]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
